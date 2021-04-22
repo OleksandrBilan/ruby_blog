3c5276303e2aeea6b6d874a6277900e58d4a7463
OleksandrBilan <79528077+OleksandrBilan@users.noreply.github.com>
2021-04-22 13:51:08 +0300

    added: authentication, article statuses, comment statuses (without archieved)

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,4 @@
-class ArticlesController < ApplicationController
+class HomeController < ApplicationController
 
   http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
 
